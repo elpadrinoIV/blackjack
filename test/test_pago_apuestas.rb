2e774bcb9636b6936dc2637c75606ea01f3467dd
Nicolas Dascanio <nd@nd.com>
2012-07-14 18:46:01 -0300

    saque los acentos en los mensajes de los assert porque sino por consola no corre

--- test/test_pago_apuestas.rb
+++ test/test_pago_apuestas.rb
@@ -20,11 +20,11 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial + @dinero_apuesta
     
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
     # me fijo que no quede plata en las apuestas
 
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 	end
 
@@ -35,11 +35,11 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial - @dinero_apuesta
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador perdió y no debería haber cobrado")
-    assert_equal(@dinero_apuesta, dinero_cobrado, "La banca debería haber cobrado porque perdió el jugador")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador perdio y no deberia haber cobrado")
+    assert_equal(@dinero_apuesta, dinero_cobrado, "La banca deberia haber cobrado porque perdio el jugador")
     # me fijo que no quede plata en las apuestas
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 	end
 
@@ -54,11 +54,11 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial + @dinero_apuesta*2
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
     # me fijo que no quede plata en las apuestas
 
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 	end
 
@@ -73,12 +73,12 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
-    assert_equal(@dinero_apuesta, dinero_cobrado, "La banca debería haber cobrado porque perdió el jugador")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
+    assert_equal(@dinero_apuesta, dinero_cobrado, "La banca deberia haber cobrado porque perdio el jugador")
 
     # me fijo que no quede plata en las apuestas
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 	end
 
@@ -92,12 +92,11 @@ class TestPagoApuestas < Test::Unit::TestCase
     
     dinero_que_deberia_tener = @dinero_inicial + @dinero_apuesta*2
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
-    # assert_equal(@dinero_apuesta, dinero_cobrado, "La banca debería haber cobrado porque perdió el jugador")
-
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
+    
     # me fijo que no quede plata en las apuestas
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 	end
 
@@ -109,12 +108,12 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial - @dinero_apuesta*2
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
-    assert_equal(@dinero_apuesta*2, dinero_cobrado, "La banca debería haber cobrado porque perdió el jugador")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
+    assert_equal(@dinero_apuesta*2, dinero_cobrado, "La banca deberia haber cobrado porque perdio el jugador")
 
     # me fijo que no quede plata en las apuestas
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 	end
 
@@ -131,15 +130,15 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial + @dinero_apuesta - @dinero_apuesta/2
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
-    assert_equal(@dinero_apuesta/2, dinero_cobrado, "La banca debería haber cobrado porque perdió el jugador")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
+    assert_equal(@dinero_apuesta/2, dinero_cobrado, "La banca deberia haber cobrado porque perdio el jugador")
 
     # me fijo que no quede plata en las apuestas
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 
-    assert_equal(0, @jugador.get_apuesta_seguro, "Después de terminar la mano no debería haber dinero en las apuestas")
+    assert_equal(0, @jugador.get_apuesta_seguro, "Despues de terminar la mano no deberia haber dinero en las apuestas")
 	end
 
   def test_jugador_paga_seguro_si_blackjack_y_pierde
@@ -155,15 +154,15 @@ class TestPagoApuestas < Test::Unit::TestCase
 
     dinero_que_deberia_tener = @dinero_inicial
 
-		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador ganó y debería haber cobrado")
-    assert_equal(@dinero_apuesta, dinero_cobrado, "La banca debería haber cobrado porque perdió el jugador")
+		assert_equal(dinero_que_deberia_tener, @jugador.get_dinero, "El jugador gano y deberia haber cobrado")
+    assert_equal(@dinero_apuesta, dinero_cobrado, "La banca deberia haber cobrado porque perdio el jugador")
 
     # me fijo que no quede plata en las apuestas
     @jugador.get_apuestas.each{ |apuesta|
-      assert_equal(0, apuesta, "Después de terminar la mano no debería haber dinero en las apuestas")
+      assert_equal(0, apuesta, "Despues de terminar la mano no deberia haber dinero en las apuestas")
     }
 
-    assert_equal(0, @jugador.get_apuesta_seguro, "Después de terminar la mano no debería haber dinero en las apuestas")
+    assert_equal(0, @jugador.get_apuesta_seguro, "Despues de terminar la mano no deberia haber dinero en las apuestas")
 	end
 
 end
